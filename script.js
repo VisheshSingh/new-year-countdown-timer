0a1ed87619d2471f1dcd24f7b7aaceeb3a99d923
Vishesh Singh <VisheshSingh16@gmail.com>
2020-02-15 12:21:05 -0600

    Create The Countdown

---
+++ script.js
@@ -0,0 +1,26 @@
+const days = document.querySelector('.days');
+const hours = document.querySelector('.hours');
+const minutes = document.querySelector('.minutes');
+const seconds = document.querySelector('.seconds');
+const container = document.getElementById('container');
+
+const currentYear = new Date().getFullYear();
+
+const newYearTime = new Date(`January 01 ${currentYear + 1} 00:00:00`);
+
+function updateTime() {
+  const currentTime = new Date();
+  const diff = newYearTime - currentTime;
+  const d = Math.floor(diff / 1000 / 60 / 60 / 24);
+  const h = Math.floor(diff / 1000 / 60 / 60) % 24;
+  const m = Math.floor(diff / 1000 / 60) % 60;
+  const s = Math.floor(diff / 1000) % 60;
+  days.textContent = d;
+  hours.textContent = h < 10 ? '0' + h : h;
+  minutes.textContent = m < 10 ? '0' + m : m;
+  seconds.textContent = s;
+}
+
+updateTime();
+
+setInterval(updateTime, 1000);
